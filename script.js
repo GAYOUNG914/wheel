9c6628f9ff268d090b229b07b6871c09f09a0386
김가영 Kim gayoung <rkdud91455@gmail.com>
2021-08-30 02:29:03 +0900

    docs: add code
    add code

--- script.js
+++ script.js
@@ -44,10 +44,23 @@ $(function () {
 
     var a = $(window).scrollTop(),
       b = $(document).height() - $(window).height(),
-      c = 0,
-      d = 100,
+      c = -50,
+      d = 50,
       valueT = (a * (d - c)) / b + c;
 
-    $(".highlight").css("width", valueT + "%");
+    $(".highlight").css("left", valueT + "%");
+  });
+
+  //Scroll top
+  $(window).on("scroll", function (e) {
+    let scTop = $(window).scrollTop();
+    let divTop1 = $(".wrap1 div:first-child").offset().top - 450;
+    let divTop2 = $(".wrap1 div:nth-child(2)").offset().top - 450;
+    let divTop3 = $(".wrap1 div:nth-child(3)").offset().top - 450;
+    let divTop4 = $(".wrap1 div:nth-child(4)").offset().top - 550;
+    scTop > divTop1 ? $(".wrap2 div:first-child").addClass("on") : $(".wrap2 div:first-child").removeClass("on");
+    scTop > divTop2 ? $(".wrap2 div:nth-child(2)").addClass("on") : $(".wrap2 div:nth-child(2)").removeClass("on");
+    scTop > divTop3 ? $(".wrap2 div:nth-child(3)").addClass("on") : $(".wrap2 div:nth-child(3)").removeClass("on");
+    scTop > divTop4 ? $(".wrap2 div:nth-child(4)").addClass("on") : $(".wrap2 div:nth-child(4)").removeClass("on");
   });
 });
